d30df47297490fee1c4430da45c15f157668ada2
안경호 <akh1216@gmail.com>
2022-09-26 16:45:58 +0900

    Three.js_commit_9.26

---
+++ three-js-webpack-master/src/index.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three'
+import { WEBGL } from './webgl'
+
+if (WEBGL.isWebGLAvailable()) {
+  
+  //장면
+  const scene = THREE.Scene();
+  scene.background = new THREE.Color(0x004fff);
+
+  //카메라
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);  
+
+  //캔버스
+  const canvas = document.querySelector('#ex-03');
+  
+  //렌더러
+  const renderer = new THREE.WebGLRenderer({canvas});
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // document.body.appendChild(renderer.domElement);
+
+
+  // renderer.render(scene, camera);
+  function render(time) {
+    time *= 0.001;  // convert time to seconds
+   
+    // cube.rotation.x = time;
+    // cube.rotation.y = time;
+   
+    renderer.render(scene, camera);
+   
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+} else {
+  var warning = WEBGL.getWebGLErrorMessage()
+  document.body.appendChild(warning)
+}
