7216a302b217879d9b09eee4786e4e7fccdb7bf7
안경호 <akh1216@gmail.com>
2022-11-23 15:25:31 +0900

    FEAT: ADD CSS3DRenderer

--- three-js-webpack-master/webpack.config.js
+++ three-js-webpack-master/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/viewer3D',
+  entry: './src/CSS2D_test',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
